f87436bbda125b32ae9610630e9f746a1e89c6a3
Jillyn Peterson <jillyn_oc_ca@yahoo.com>
2020-11-16 14:01:03 -0700

    josh, help???

--- week5/day2/lets-make-art/server/index.js
+++ week5/day2/lets-make-art/server/index.js
@@ -6,10 +6,10 @@ const express = require("express"),
 app.use(express.json());
 
 
-app.get("/api/watercolor", ctrl.displayAll);
-app.get("/api/watercolor/:id", ctrl.getProjects);
-app.post("/api/projects/:id", ctrl.addToProj);
-app.put("/api/projects/:index", ctrl.editProj);
-app.delete("/api/projects/:index", ctrl.deleteProj);
+app.get('/api/watercolor', ctrl.displayAll);
+app.get('/api/watercolor/:id', ctrl.getProjects);
+app.post('/api/projects/:id', ctrl.addToProj);
+app.put('/api/projects/:id', ctrl.editProj);
+app.delete('/api/projects/:id', ctrl.deleteProj);
 
 app.listen(port, () => console.log(`It's working! Who knew??? I'm listening on port ${port}`));
